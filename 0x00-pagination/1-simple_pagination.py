3f7bce5e454750e7b9929067e041fc4eb905d44b
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-06 15:20:15 +0100

    task1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,7 +2,8 @@
 """tuple of size two containing a start index and an end index"""
 
 import csv
-from typing import List
+import math
+from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
@@ -17,30 +18,32 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
 
 
 class Server:
-    """ class init with dataset"""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """arguments are integers greater than 0"""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
+
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Page must be a positive integer"""
-        assert (
-                isinstance(page, int) and page > 0
-              ), "Page must be a positive integer"
-        assert (
-                isinstance(page_size, int) and page_size > 0
-               ), "Page size must be a positive integer"
-
-        dataset = self.dataset()
+        """
+        Return a dictionary with hypermedia pagination.
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
-        return dataset[start:end]
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
