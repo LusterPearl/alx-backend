d1680a66f550d8f1897b8a607a07d5177b30672a
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-06 15:27:57 +0100

    tsk

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -6,7 +6,7 @@ import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
