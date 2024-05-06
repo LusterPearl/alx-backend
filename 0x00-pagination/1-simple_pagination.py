469878aa6f615838953772ee6e33f69a344cd2d3
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-06 13:42:14 +0100

    pep2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -17,22 +17,29 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
 
 
 class Server:
+    """ class init with dataset"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """arguments are integers greater than 0"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]  # Skip header row
+            self.__dataset = dataset[1:]
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0, "Page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer"
+        """Page must be a positive integer"""
+        assert (
+    isinstance(page, int) and page > 0
+              ), "Page must be a positive integer"
+        assert (
+    isinstance(page_size, int) and page_size > 0
+               ), "Page size must be a positive integer"
 
         dataset = self.dataset()
         start, end = index_range(page, page_size)
