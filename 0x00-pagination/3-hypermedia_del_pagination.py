244793f2b5af0a4918d186c3c717264040b3177c
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-06 13:41:48 +0000

    stillon

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -38,7 +38,7 @@ class Server:
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
-    
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Return a dictionary with hypermedia pagination information.
