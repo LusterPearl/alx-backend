97cad7d97a397acbb7d091434ccb58b918be4d24
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-08 12:24:43 +0000

    tas222

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,7 +2,6 @@
 """Module for LIFOCache class."""
 
 from base_caching import BaseCaching
-from collections import deque
 
 
 class LIFOCache(BaseCaching):
@@ -12,7 +11,7 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Initialize the LIFOCache."""
         super().__init__()
-        self.insertion_order = deque()
+        self.insertion_order = []
 
     def put(self, key, item):
         """Add an item to the cache."""
@@ -25,13 +24,11 @@ class LIFOCache(BaseCaching):
             print("DISCARD:", discarded_key)
 
         self.cache_data[key] = item
-        self.insertion_order.appendleft(key)
+        self.insertion_order.append(key)
 
     def get(self, key):
         """Retrieve an item from the cache."""
         if key is None or key not in self.cache_data:
             return None
 
-        self.insertion_order.remove(key)
-        self.insertion_order.appendleft(key)
         return self.cache_data[key]
