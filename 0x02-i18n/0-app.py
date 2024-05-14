6c18b8bdc852edd5e65ca902a1f921dd224d8092
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-14 12:31:03 +0000

    py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+from flask import Flask, render_templates
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    Route for the index page
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__:
+    app.run(debug=True)
