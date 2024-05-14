1b1a824a2143044be2e5bed589b23b710aadfa6f
LusterPearl <Salamioluwatosin90@gmail.com>
2024-05-14 13:04:24 +0000

    2py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Flask app with Babel locale selection"""
+from flask import Flask, render_template, request
+from fask_babel import Babel
+
+
+class config:
+    """
+    configuratio for flask Babel
+    """
+    LANGUAGES = ["en" "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match ith supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES']
+
+@app.route('/')
+def index():
+    """route for the index page"""
+    return render_template('2_index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
